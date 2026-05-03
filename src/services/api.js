52fb2321962e95f933c95e009f65cbae8e33ebca
web-devs <mohammedshameem636@gmail.com>
2026-05-03 17:31:59 +0530

    Employee Management System

--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,4 @@ const API = "https://69f7306ddd0c226688ed8469.mockapi.io/Employee"
 export const getEmployees = () => axios.get(API)
 export const addEmployee = (data) => axios.post(API, data)
 export const updateEmployee = (id, data) => axios.put(`${API}/${id}`, data)
-export const deleteEmployee = (id) => axios.delete(`${API}/${id}`)
+export const deleteEmployee = (id) => axios.delete(`${API}/${id}
